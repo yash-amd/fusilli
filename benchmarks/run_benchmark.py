5dccb9fae08324d24cc65d96ddf513e9be6b6982
Yash Rathore <yrathore@amd.com>
2026-04-07 12:15:41 +0530

    Update run_benchmark.py

--- benchmarks/run_benchmark.py
+++ benchmarks/run_benchmark.py
@@ -165,6 +165,26 @@ def run_profiled_command(
 
     try:
         print(">>> [DEBUG 14] __main__ block reached", flush=True)
+        result_version = subprocess.run(
+          ["rocprofv3", "--version"],
+          capture_output=True,
+          text=True
+      )
+      print("=== rocprofv3 --version ===")
+      print(result_version.stdout)
+      print(result_version.stderr)
+    
+      # Run which rocprofv3
+      result_which = subprocess.run(
+          ["which", "rocprofv3"],
+          capture_output=True,
+          text=True
+      )
+      print("\n=== which rocprofv3 ===")
+      print(result_which.stdout.strip())
+      sys.exit(1)
+
+
         rocprof_cmd = (
             [
                 "rocprofv3",
